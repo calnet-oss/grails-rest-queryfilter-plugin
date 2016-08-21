2d19f3b69c8ad36e9731182cda965609630b261f
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2016-08-21 09:09:27 -0700

    Convert to Gradle

--- RestQueryfilterGrailsPlugin.groovy
+++ RestQueryfilterGrailsPlugin.groovy
@@ -1,8 +1,9 @@
 class RestQueryfilterGrailsPlugin {
     def group = "edu.berkeley.calnet.plugins"
+    //def group = "edu.berkeley.calnet.grails.plugins"
 
     // the plugin version
-    def version = "0.3-SNAPSHOT"
+    def version = "0.4-SNAPSHOT" // !!! Change in build.gradle too
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
@@ -39,33 +40,23 @@ Handle query filter strings for REST queries
 //    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
 
     def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
     }
 
     def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
     }
 
     def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
     }
 
     def doWithApplicationContext = { ctx ->
-        // TODO Implement post initialization spring config (optional)
     }
 
     def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
     }
 
     def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
     }
 
     def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
     }
 }
