f4d3700a88dda48eda92e2de2395e89f5807f160
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2016-08-21 09:28:12 -0700

    Add license

--- RestQueryfilterGrailsPlugin.groovy
+++ RestQueryfilterGrailsPlugin.groovy
@@ -1,17 +1,15 @@
 class RestQueryfilterGrailsPlugin {
-    def group = "edu.berkeley.calnet.plugins"
-    //def group = "edu.berkeley.calnet.grails.plugins"
+    def group = "edu.berkeley.calnet.grails.plugins"
 
     // the plugin version
-    def version = "0.4-SNAPSHOT" // !!! Change in build.gradle too
+    def version = "1.0.0-SNAPSHOT" // !!! Change in build.gradle too
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-        "grails-app/views/error.gsp"
+            "grails-app/views/error.gsp"
     ]
 
-    // TODO Fill in these fields
     def title = "REST query filter Plugin" // Headline display name of the plugin
     def author = "Brian Koehmstedt"
     def authorEmail = "bkoehmstedt@berkeley.edu"
@@ -20,24 +18,24 @@ Handle query filter strings for REST queries
 '''
 
     // URL to the plugin's documentation
-    //def documentation = "http://grails.org/plugin/rest-queryfilter"
+    def documentation = "https://github.com/calnet-oss/grails-rest-queryfilter-plugin/"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "BSD"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [name: "University of California, Berkeley", url: "http://www.berkeley.edu/"]
 
     // Any additional developers beyond the author specified above.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [system: "GitHub", url: "https://github.com/calnet-oss/grails-rest-queryfilter-plugin/issues"]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [url: "https://github.com/calnet-oss/grails-rest-queryfilter-plugin"]
 
     def doWithWebDescriptor = { xml ->
     }
