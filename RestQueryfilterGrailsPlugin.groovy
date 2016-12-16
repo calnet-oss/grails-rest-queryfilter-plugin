72bc4d0638caf90e9297699c28e94ac2fe2605a0
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2016-12-16 11:07:49 -0800

    Upgrade gradle-grails-maven-publish-plugin, gradle wrapper, and publish source and class JARs.

--- RestQueryfilterGrailsPlugin.groovy
+++ RestQueryfilterGrailsPlugin.groovy
@@ -28,7 +28,7 @@ class RestQueryfilterGrailsPlugin {
     def group = "edu.berkeley.calnet.grails.plugins"
 
     // the plugin version
-    def version = "1.0.0-SNAPSHOT" // !!! Change in build.gradle too
+    def version = "1.0.1-SNAPSHOT" // !!! Change in build.gradle too
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
